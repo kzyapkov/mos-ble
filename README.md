023eb577cfdaab6085ad848951076137f606d704
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2021-01-18 20:30:05 +0200

    Re-implement with bleak

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # mos-ble
 
-This is a basic tool to test Mongoose-OS RPC over GATTS
-
-Needs `gatt-python` from https://github.com/getsenic/gatt-python
+This is a basic tool to test Mongoose-OS RPC over GATTS.
--- mos_ble.py
+++ mos_ble.py
@@ -1,226 +1,170 @@
 #!/usr/bin/env python3
-"""
 
-https://github.com/getsenic/gatt-python
-
-
-mos-ble scan
-mos-ble call method [payload]
-mos-ble logcat
-
-"""
-import sys
-import os
-import re
 import argparse
-import functools
+import asyncio
 import json
 import logging
+import os
+import re
+import sys
 import struct
-import threading
+from dataclasses import dataclass
 
-import gatt
+from bleak import BleakScanner, BleakClient
+
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.WARNING)
 
-mos_rpc_uuid = '5f6d4f53-5f52-5043-5f53-56435f49445f'
-data_uuid = '5f6d4f53-5f52-5043-5f64-6174615f5f5f'
-tx_ctl_uuid = '5f6d4f53-5f52-5043-5f74-785f63746c5f'
-rx_ctl_uuid = '5f6d4f53-5f52-5043-5f72-785f63746c5f'
+mos_rpc_uuid = "5f6d4f53-5f52-5043-5f53-56435f49445f"
+data_uuid = "5f6d4f53-5f52-5043-5f64-6174615f5f5f"
+tx_ctl_uuid = "5f6d4f53-5f52-5043-5f74-785f63746c5f"
+rx_ctl_uuid = "5f6d4f53-5f52-5043-5f72-785f63746c5f"
+
 
 def bda_arg(s):
     if not re.match(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", s):
         raise argparse.ArgumentTypeError
     return s
 
+
 def get_argparser():
-    parser = argparse.ArgumentParser(description='Mongoose RPC over BLE')
-    parser.add_argument(
-            "--adapter", type=str, default='hci0')
+    parser = argparse.ArgumentParser(description="Mongoose RPC over BLE")
+    parser.add_argument("--adapter", type=str, default="hci0")
+
     subparsers = parser.add_subparsers(dest="cmd")
 
-    parser_scan = subparsers.add_parser(
-            "scan", help="Scan for nearby BLE devices")
+    parser_scan = subparsers.add_parser("scan", help="Scan for nearby BLE devices")
     parser_scan.add_argument(
-            "--timeout", type=int, default=5, help="Scan time, seconds")
+        "--timeout", type=int, default=5, help="Scan time, seconds"
+    )
 
-    parser_call = subparsers.add_parser(
-            "call", help="Invoke a RPC method")
+    parser_call = subparsers.add_parser("call", help="Invoke a RPC method")
+    parser_call.add_argument("method", type=str)
     parser_call.add_argument(
-            "method", type=str)
-    parser_call.add_argument(
-            "--address", type=bda_arg,
-            default=(os.environ.get('MOS_BLE_ADDR', "")),
-            help="BLE device address to connet to, required still")
-    parser_call.add_argument(
-            "--args", type=str, default="null")
+        "-a",
+        "--address",
+        type=bda_arg,
+        default=(os.environ.get("MOS_BLE_ADDR", "")),
+        help="BLE device address to connet to, required",
+    )
+    parser_call.add_argument("params", type=str, nargs="?", default=None)
+
     return parser
 
 
-class MosDevice(gatt.Device):
+async def scan():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
 
-    def __init__(self, mac_address, manager):
-        super().__init__(mac_address=mac_address, manager=manager)
-        self._id = 99
-        self._on_ready = None
-        self._incoming_len = 0
-        self._incoming_data = b''
-        self._payload = b''
+@dataclass
+class RPCCall:
+    id: int
+    method: str
+    params: dict = None
+    src: str = 'mos-ble'
+    dst: str = None
+    resolve: asyncio.Future = None
 
-    def _get_id(self):
-        self._id += 1
-        return self._id
+    def __init__(self, id: int, method: str, params=None):
+        self.id = id
+        self.method = method
+        self.params = params
+        self.resp = bytearray()
+        self.resp_len = -1
 
-    def run_rpc_call(self, method, args=None):
-
-        frame = {
-            "id": self._get_id(),
-            "method": method,
-            "src": "mos-ble",
-            "args": args,
+    @property
+    def out_msg(self):
+        msg = {
+            'id': self.id,
+            'method': self.method,
+            'src': self.src,
         }
+        for o in ('params', 'src', 'dst'):
+            if getattr(self, o) is not None:
+                msg[o] = getattr(self, o)
+        return msg
 
-        self._payload = json.dumps(frame)
+    @property
+    def request_json(self):
+        return json.dumps(self.out_msg)
 
-        def _on_ready(self):
-            frame_len = len(self._payload)
-            log.info("sending %d bytes", frame_len)
-            self.tx_ctl_ch.write_value(struct.pack(">I", frame_len))
 
-        if (self.is_services_resolved):
-            _on_ready(self)
+async def call(address, method, params=None):
+    async with BleakClient(address) as client:
+        x = await client.is_connected()
+        log.debug("Connected: {0}".format(x))
+
+        svcs = await client.get_services()
+        if mos_rpc_uuid not in svcs.services:
+            log.error(f"{address} doesn't contain the mgos RPC service")
+            return
+
+        call = RPCCall(99, method, params)
+
+        req = bytearray(map(ord, call.request_json))
+        req_len = bytearray(4)
+        struct.pack_into(">I", req_len, 0, len(req))
+
+        await client.write_gatt_char(tx_ctl_uuid, req_len)
+        await client.write_gatt_char(data_uuid, req)
+
+        # FIXME:
+        # subscribing for notifications on rx_ctl_uuid doesn't work
+        # so let's just poll for now
+        for _ in range(5):
+            res = await client.read_gatt_char(rx_ctl_uuid)
+            if len(res) == 4:
+                resp_len = struct.unpack(">I", res)[0]
+                if resp_len != 0:
+                    call.resp_len = resp_len
+                    break
+
+        if call.resp_len <= 0:
+            log.warning("Did not get a response")
+            return
+
+        log.info(f"got {resp_len} bytes to read")
+        bytes_left = call.resp_len
+        while bytes_left > 0:
+            data = await client.read_gatt_char(data_uuid)
+            call.resp.extend(data)
+            bytes_left -= len(data)
+
+        log.info(f"resp = {call.resp}")
+        try:
+            resp = json.loads(call.resp)
+        except json.JSONDecodeError as e:
+            log.error(f"response not valid JSON: {e}")
+            return
+
+        if 'error' in resp:
+            print(json.dumps(resp['error'], indent=4, sort_keys=True))
+        elif 'result' in resp:
+            print(json.dumps(resp['result'], indent=4, sort_keys=True))
         else:
-            self._on_ready = functools.partial(_on_ready, self)
-            log.info("prepared for call: %s", self._on_ready)
-
-        self.manager.run()
-
-    def connect_succeeded(self):
-        super().connect_succeeded()
-        log.info("[%s] Connected", self.mac_address)
-
-    def connect_failed(self, error):
-        super().connect_failed(error)
-        log.error("[%s] Connection failed: %s", self.mac_address, str(error))
-        self.manager.stop()
-
-    def disconnect_succeeded(self):
-        super().disconnect_succeeded()
-        log.info("[%s] Disconnected", self.mac_address)
-        self.manager.stop()
-
-    def services_resolved(self):
-        super().services_resolved()
-
-        log.debug("[%s] Resolved services", self.mac_address)
-        for service in self.services:
-            log.debug("[%s]  Service [%s]" % (self.mac_address, service.uuid))
-            for characteristic in service.characteristics:
-                log.debug("[%s]    Characteristic [%s]" % (self.mac_address, characteristic.uuid))
-
-        self.mos_rpc_service = next(
-            s for s in self.services
-            if s.uuid == mos_rpc_uuid)
-        self.data_ch = next(
-            c for c in self.mos_rpc_service.characteristics
-            if c.uuid == data_uuid)
-        self.tx_ctl_ch = next(
-            c for c in self.mos_rpc_service.characteristics
-            if c.uuid == tx_ctl_uuid)
-        self.rx_ctl_ch = next(
-            c for c in self.mos_rpc_service.characteristics
-            if c.uuid == rx_ctl_uuid)
-
-        self.rx_ctl_ch.enable_notifications()
-
-        if callable(self._on_ready):
-            log.info("_on_ready")
-            self._on_ready()
-
-    def characteristic_write_value_succeeded(self, characteristic):
-        log.debug("characteristic_write_value_succeeded on %s", characteristic.uuid)
-        if characteristic.uuid == tx_ctl_uuid:
-            log.debug("sending frame: %s", self._payload)
-            self.data_ch.write_value([ord(x) for x in self._payload])
-        if characteristic.uuid == data_uuid:
-            log.debug("frame sent, waiting for response")
-
-    def characteristic_write_value_failed(self, characteristic, error):
-        log.warn("characteristic_write_value_failed on %s: %s", characteristic.uuid, error)
-        self.manager.stop()
-
-    def characteristic_value_updated(self, characteristic, value):
-        if (characteristic.uuid == rx_ctl_uuid):
-            in_len = struct.unpack(">I", value)[0]
-            if self._incoming_len == 0:
-                self._incoming_len = in_len
-                if self._incoming_len:
-                    log.debug("reading %d bytes ..." % self._incoming_len)
-                    self.data_ch.read_value()
-            else:
-                log.info("got {} for incoming, but already have {}".format(
-                         in_len, self._incoming_len))
-
-        if (characteristic.uuid == data_uuid):
-            data_len = len(value)
-            self._incoming_len -= data_len
-            self._incoming_data += value
-            log.debug("got {} bytes, {} remaining".format(data_len, self._incoming_len))
-            if self._incoming_len > 0:
-                self.data_ch.read_value()
-            else:
-                lfunc = log.debug if self._incoming_len == 0 else log.warn
-                lfunc("incoming_len={} actual={}".format
-                        (self._incoming_len, len(self._incoming_data)))
-                try:
-                    data = json.loads(self._incoming_data)
-                    if ('result' in data):
-                        print(json.dumps(data['result']))
-                    elif ('error' in data):
-                        print(json.dumps(data['error']))
-                    else:
-                        log.warn("Seemingly bad response: %s", self._incoming_data)
-
-                except Exception as err:
-                    log.exception("BAD JSON: {}".format(err))
-                    log.debug("data: %s", self._incoming_data)
-                finally:
-                    self.manager.stop()
-
-
-class ScanDeviceManager(gatt.DeviceManager):
-
-    def __init__(self, adapter_name, timeout=None):
-        self._devs = {}
-        if timeout is not None:
-            threading.Timer(timeout, self.stop).start()
-        super(ScanDeviceManager, self).__init__(adapter_name=adapter_name)
-
-    def device_discovered(self, device):
-        k = (device.mac_address, device.alias())
-        if k not in self._devs:
-            print("%s %s" % k)
-            self._devs[k] = True
+            log.error(f"invalid response frame: {resp}")
 
 
 def main():
     p = get_argparser()
     args = p.parse_args(sys.argv[1:])
 
-    if args.cmd == 'scan':
-        manager = ScanDeviceManager(adapter_name=args.adapter, timeout=args.timeout)
-        manager.start_discovery()
-        manager.run()
+    coro = None
 
-    elif args.cmd == 'call':
-        if args.args:
-            args.args = json.loads(args.args)
-        manager = gatt.DeviceManager(adapter_name=args.adapter)
-        device = MosDevice(mac_address=args.address, manager=manager)
-        device.connect()
-        if device.is_connected() and device.is_services_resolved():
-            device.run_rpc_call(args.method, args.args)
+    if args.cmd == "scan":
+        coro = scan()
 
-if __name__ == '__main__':
+    elif args.cmd == "call":
+        if args.params:
+            args.params = json.loads(args.params)
+        coro = call(args.address, args.method, args.params)
+
+    if coro:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(coro)
+
+
+if __name__ == "__main__":
     main()
